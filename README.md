98c266a2ec004bb3eabda23643e3c35b06cff198
Pawel Skwirowski <pawel.skwirowski@tieto.com>
2021-03-25 13:56:29 +0100

    Add express-validator and update examples
     - added express-validator library for inputs validation
     - added example code
     - updated README

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Libraries:
 
-1. Express - minimalist web framework for Node.js
-2. nodemon - monitors changes in the source code and automatically restarts the server
+1. [Express](https://expressjs.com/) - minimalist web framework for Node.js
+2. [nodemon](https://nodemon.io/) - monitors changes in the source code and automatically restarts the server
+3. [express-validator](https://express-validator.github.io/docs/) - express.js middleware for inputs validation
+
+# Useful programs:
+
+1. [Postman](https://www.postman.com/) - tool for testing API endpoints
--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+// Test data
+const books = [
+    { id: 1, name: 'book 1' },
+    { id: 2, name: 'book 2' },
+    { id: 3, name: 'book 3' }
+];
+
+// Test routes
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
+app.get('/api/posts', (req, res) => {
+    res.send(req.query);
+});
+
+// Handle HTTP GET request
+app.get('/api/books/:id', (req, res) => {
+    const book = books.find(book => book.id === +req.params.id);
+    if (!book) {
+        res.status(404).send('The book with given ID was not found');
+    }
+    res.send(book);
+});
+
+// Handle HTTP POST request
+app.post('/api/books', (req, res) => {
+    const book = {
+        id: books.length + 1,
+        name: req.body.name
+    };
+
+    books.push(book);
+    res.send(book);
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`));
