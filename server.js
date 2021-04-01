659b4911c8aec74031b4423c8d7da7c3788c909b
jczujko <jarek.czujko@hotmail.com>
2021-04-01 15:53:24 +0200

    swagger installed

--- server.js
+++ server.js
@@ -18,6 +18,31 @@ const PORT = process.env.PORT || 8080
 
 app.use('/api/users-sample', usersSampleRoutes)
 
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Patronage JS API',
+    version: '1.0.0'
+  },
+  servers: [
+    {
+      url: 'http://localhost:8080/api/users-sample',
+      description: 'Development server'
+    }
+  ]
+}
+
+const swaggerOptions = {
+  swaggerDefinition,
+  apis: ['./app/routes/*.js']
+}
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 app.listen(PORT, () => {
   console.log(`Server started on port http://127.0.0.1:${PORT}`)
 
