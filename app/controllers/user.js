9ea1665a525cbdf33aa0ce6831addd39a632ce5f
jczujko <jarek.czujko@hotmail.com>
2021-04-15 13:26:33 +0200

    IP2-244 controller refactor

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,11 +1,16 @@
-const {
-  validationResult
-} = require('express-validator')
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const Ajv = require('ajv')
+const addFormats = require('ajv-formats')
+
+const ajv = new Ajv({ allErrors: true })
+addFormats(ajv)
+ajv.addVocabulary(['swagger', 'info', 'basePath', 'tags', 'paths'])
+const swaggerSchema = require('../../swagger.json')
+ajv.addSchema(swaggerSchema, 'swagger.json')
 
 const register = async (req, res, next) => {
-  const errors = {
+  /* const errors = {
     fields: {},
     general: []
   }
@@ -69,8 +74,56 @@ const register = async (req, res, next) => {
   } catch (err) {
     return res.status(500).send('Nieudana rejestracja').end()
   }
+  */
+  const {
+    gender,
+    name,
+    surname,
+    email,
+    phone,
+    technologies,
+    password: notHashedPassport
+  } = req.body
 
-  res.status(200).end()
+  const validateUser = ajv.compile({ $ref: 'swagger.json#/definitions/User' })
+  const valid = validateUser(req.body)
+  if (!valid) {
+    // Wysyła domyślną tablicę błędów generowaną przez ajv, wymaga modyfikacji
+    return res.status(400).send(validateUser.errors).end()
+  } else {
+    let existingUser
+    try {
+      existingUser = await User.findOne({
+        email: email
+      })
+    } catch (err) {
+      return res.status(500).send('Nieudana rejestracja').end()
+    }
+
+    let password
+    try {
+      password = await bcrypt.hash(notHashedPassport, 12)
+    } catch (err) {
+      return res.status(500).send('Nieudana rejestracja').end()
+    }
+
+    const createdUser = new User({
+      gender,
+      name,
+      surname,
+      email,
+      phone,
+      technologies,
+      password
+    })
+
+    try {
+      await createdUser.save()
+    } catch (err) {
+      return res.status(500).send('Nieudana rejestracja').end()
+    }
+  }
+  res.status(200).send('Udana rejestracja').end()
 }
 
 exports.register = register
