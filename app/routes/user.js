a216ad27945386a5651e43de6ee0d6c92de10001
Ryczko <konradryczko1@gmail.com>
2021-05-02 17:41:39 +0200

    IP2-294: users list

--- app/routes/user.js
+++ app/routes/user.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.post('/register', usersController.register)
 router.put('/activate', usersController.activateUser)
+router.get('/list', usersController.listOfUsers)
 
 module.exports = router
