32d1aab98aebc9698666eccba7a9f33ea5cfd818
Konrad Ryczko <konradryczko1@gmail.com>
2021-06-03 11:58:00 +0200

    IP2-564: edit event (#30)

--- app/routes/events.js
+++ app/routes/events.js
@@ -5,6 +5,7 @@ const eventsController = require('../controllers/events')
 const router = express.Router()
 
 router.post('/', eventsController.addEvent)
+router.patch('/:id', eventsController.patchEvent)
 router.delete('/delete/:id', eventsController.deleteEvent)
 
 module.exports = router
